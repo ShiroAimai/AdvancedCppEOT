4e12f3c42cdfff5e3d90defaef866fd73649fcec
Nick <shiroaimai@gmail.com>
2021-08-25 17:14:20 +0200

    Issue with small obj allocation memory tracking

--- ShirosMemoryManager/SmallObjAllocator.cpp
+++ ShirosMemoryManager/SmallObjAllocator.cpp
@@ -25,28 +25,27 @@ void* SmallObjAllocator::Allocate(const size_t bytes, size_t& OutAllocatedMemory
 	OutAllocatedMemory = bytes; //we allocate just the right amount of memory
 
 	//check if we already allocated an allocator, and if this one manage Chunk of the desired size
-	if (m_lastAllocatorUsedForAllocation && m_lastAllocatorUsedForAllocation->GetBlockSize() == bytes)
+	if (!m_lastAllocatorUsedForAllocation || m_lastAllocatorUsedForAllocation->GetBlockSize() != bytes)
 	{
-		return m_lastAllocatorUsedForAllocation->Allocate();
+		//find the correct allocator in list
+		AllocatorPool::iterator it = std::lower_bound(m_Pool.begin(), m_Pool.end(), bytes, FixedAllocatorComparator);
+
+		// if the allocator that manage this size_t is nowhere to be found 
+		// or the one we found does not manage the exact sizeof bytes
+		// then insert a new Allocator that'll do the work
+		if (it == m_Pool.end() || it->GetBlockSize() != bytes)
+		{
+			it = m_Pool.insert(it, FixedAllocator(m_chunkSize, bytes));
+			m_lastAllocatorUsedForDeallocation = &*m_Pool.begin();
+		}
+		
+		//either we found an allocator or we're using the one we just inserted
+		m_lastAllocatorUsedForAllocation = &*it;
 	}
 
-	//find the correct allocator in list
-	AllocatorPool::iterator it = std::lower_bound(m_Pool.begin(), m_Pool.end(), bytes, FixedAllocatorComparator);
-
-	// if the allocator that manage this size_t is nowhere to be found 
-	// or the one we found does not manage the exact sizeof bytes
-	// then insert a new Allocator that'll do the work
-	if (it == m_Pool.end() || it->GetBlockSize() != bytes)
-	{
-		it = m_Pool.insert(it, FixedAllocator(m_chunkSize, bytes));
-		m_lastAllocatorUsedForDeallocation = &*m_Pool.begin();
-	}
-	
-	//either we found an allocator or we're using the one we just inserted
-	m_lastAllocatorUsedForAllocation = &*it;
-	m_totMemoryAllocated -= it->GetTotalAllocatedMemory(); //remove current memory occupied by it that does not consider new allocation
+	m_totMemoryAllocated -= m_lastAllocatorUsedForAllocation->GetTotalAllocatedMemory(); //remove current memory occupied by it that does not consider new allocation
 	void* p_res = m_lastAllocatorUsedForAllocation->Allocate();
-	m_totMemoryAllocated += it->GetTotalAllocatedMemory(); //add new value for memory occupied by it also considering new allocation
+	m_totMemoryAllocated += m_lastAllocatorUsedForAllocation->GetTotalAllocatedMemory(); //add new value for memory occupied by it also considering new allocation
 	return p_res;
 }
 
